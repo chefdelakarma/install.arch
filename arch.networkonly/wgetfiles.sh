6a1ce73587cedb1f514412f35a8dd0510ac469ee
Le chef de la karma <159172026+chefdelakarma@users.noreply.github.com>
2024-10-11 14:23:45 +0200

    Add files via upload

--- arch.networkonly/wgetfiles.sh
+++ arch.networkonly/wgetfiles.sh
@@ -4,8 +4,6 @@
 # LICENSE:		GNU GPLv2
 file=${file:-getfiles}
 cat $file | sed '/^$/d; /^#/d' | while read -r from to; do
-	wget $from
-	filename=$(basename $from)
 	[[ -d $to ]] || sudo mkdir -pv $to
-	sudo cp $filename $to
+	sudo wget "$from" -P "$to"
 done
