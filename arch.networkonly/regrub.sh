b93fd36361141fec65f64adb4453be3b35ad5758
Le chef de la karma <159172026+chefdelakarma@users.noreply.github.com>
2025-07-09 16:05:23 +0200

    Add files via upload

--- arch.networkonly/regrub.sh
+++ arch.networkonly/regrub.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 root=${1:-"/dev/nvme0n1p4"}
-mnt=${2:-"/mnt"}
+mnt=${mnt:-"/mnt"}
 
 mount ${root} ${mnt}
 for i in /dev /dev/pts /proc /sys /run
@@ -9,10 +9,16 @@ do mount --bind $i ${mnt}$i
 done
 
 chroot ${mnt} /bin/bash << 'EOF'
-	mount -a
+	mount -av
 	sleep 30
-	mount -t efivarfs efivarfs /sys/firmware/efi/efivars
+	mount -v -t efivarfs efivarfs /sys/firmware/efi/efivars
 	sleep 30
-	grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+	grub-install -v --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 	grub-mkconfig -o /boot/grub/grub.cfg
+	umount -v /sys/firmware/efi/efivars
 EOF
+
+for i in /dev/pts /dev /proc /sys /run
+do umount -v ${mnt}$i
+done
+
