3b1c104d3f6f644a43fca7528baf6012d227aaed
Le chef de la karma <159172026+chefdelakarma@users.noreply.github.com>
2025-07-16 12:42:56 +0200

    Update regrub.sh

--- arch.networkonly/regrub.sh
+++ arch.networkonly/regrub.sh
@@ -12,7 +12,7 @@ mount -v -t efivarfs efivarfs ${mnt}/sys/firmware/efi/efivars
 
 chroot ${mnt} /bin/bash << 'EOF'
 	mount -av
-	sleep 30
+	sleep 5
 	grub-install -v --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 	grub-mkconfig -v -o /boot/grub/grub.cfg
 EOF
