b9e5e6345c1dbb45db08b9535847402f0b759c00
Le chef de la karma <159172026+chefdelakarma@users.noreply.github.com>
2025-08-05 09:01:50 +0200

    Update regrub.sh

--- arch.networkonly/regrub.sh
+++ arch.networkonly/regrub.sh
@@ -19,12 +19,5 @@ chroot ${mnt} /bin/bash << 'EOF'
 	grub-mkconfig -v -o /boot/grub/grub.cfg
 EOF
 
-for i in /sys/firmware/efi/efivars /dev/pts /dev /proc /sys /run
-do umount -v ${mnt}$i
-done
-for i in $(awk '!/^#/ && NF {print $2}' ${mnt}/etc/fstab | sort -r); do
-	if [[ $i != "none" ]]; then
-		umount -v ${mnt}$i
-	fi
-done
+umount -R ${mnt}
 
